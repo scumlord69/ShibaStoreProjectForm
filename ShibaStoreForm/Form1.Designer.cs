72ce99fdd4af2816456b1ec7be26319c2edb5e7c
Niels-oliver Eriksson <nier0009@student.hv.se>
2018-10-24 13:41:52 +0200

    Add project files.

---
+++ ShibaStoreForm/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace ShibaStoreForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.updateBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dognametxt = new System.Windows.Forms.TextBox();
+            this.dogdescriptiontxt = new System.Windows.Forms.TextBox();
+            this.dogimagetxt = new System.Windows.Forms.TextBox();
+            this.createBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(372, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(372, 144);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(416, 294);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // updateBtn
+            // 
+            this.updateBtn.Location = new System.Drawing.Point(453, 115);
+            this.updateBtn.Name = "updateBtn";
+            this.updateBtn.Size = new System.Drawing.Size(75, 23);
+            this.updateBtn.TabIndex = 2;
+            this.updateBtn.Text = "Update dog";
+            this.updateBtn.UseVisualStyleBackColor = true;
+            this.updateBtn.Click += new System.EventHandler(this.updateBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Location = new System.Drawing.Point(534, 115);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(75, 23);
+            this.deleteBtn.TabIndex = 3;
+            this.deleteBtn.Text = "Delete dog";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Dog name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dog description";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 254);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Dog image name";
+            // 
+            // dognametxt
+            // 
+            this.dognametxt.Location = new System.Drawing.Point(15, 172);
+            this.dognametxt.Name = "dognametxt";
+            this.dognametxt.Size = new System.Drawing.Size(283, 20);
+            this.dognametxt.TabIndex = 7;
+            // 
+            // dogdescriptiontxt
+            // 
+            this.dogdescriptiontxt.Location = new System.Drawing.Point(15, 223);
+            this.dogdescriptiontxt.Name = "dogdescriptiontxt";
+            this.dogdescriptiontxt.Size = new System.Drawing.Size(283, 20);
+            this.dogdescriptiontxt.TabIndex = 8;
+            // 
+            // dogimagetxt
+            // 
+            this.dogimagetxt.Location = new System.Drawing.Point(15, 274);
+            this.dogimagetxt.Name = "dogimagetxt";
+            this.dogimagetxt.Size = new System.Drawing.Size(283, 20);
+            this.dogimagetxt.TabIndex = 9;
+            // 
+            // createBtn
+            // 
+            this.createBtn.Location = new System.Drawing.Point(15, 313);
+            this.createBtn.Name = "createBtn";
+            this.createBtn.Size = new System.Drawing.Size(75, 23);
+            this.createBtn.TabIndex = 10;
+            this.createBtn.Text = "Save dog";
+            this.createBtn.UseVisualStyleBackColor = true;
+            this.createBtn.Click += new System.EventHandler(this.createBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.createBtn);
+            this.Controls.Add(this.dogimagetxt);
+            this.Controls.Add(this.dogdescriptiontxt);
+            this.Controls.Add(this.dognametxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.updateBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button updateBtn;
+        private System.Windows.Forms.Button deleteBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox dognametxt;
+        private System.Windows.Forms.TextBox dogdescriptiontxt;
+        private System.Windows.Forms.TextBox dogimagetxt;
+        private System.Windows.Forms.Button createBtn;
+    }
+}
+
---
+++ ShibaStoreForm/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShibaStoreForm
+{
+    public partial class Form1 : Form
+    {
+        StoreDBEntities db = new StoreDBEntities();
+        public Form1()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.Dogs.ToList();
+        }
+
+        private void updateBtn_Click(object sender, EventArgs e)
+        {
+            db.SaveChanges();
+            MessageBox.Show("Data updated! :)");
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+
+            if(dataGridView1.CurrentRow == null)
+            {
+                MessageBox.Show("No row selected. :(");
+            }
+            else
+            {
+                string selectedRow = dataGridView1.CurrentRow.Cells[2].FormattedValue.ToString();
+                int selectedID = Int32.Parse(selectedRow);
+
+
+
+                foreach (var user in db.Users)
+                {
+                    User users = db.Users.First(i => i.UserID == user.UserID);
+                    Dog dog = db.Dogs.First(i => i.DogID == selectedID);
+
+                    users.Dogs.Remove(dog);
+                    db.Dogs.Remove(dog);
+                }
+                db.SaveChanges();
+                MessageBox.Show("Data deleted! :)");
+                dataGridView1.DataSource = db.Dogs.ToList();
+
+            }
+        }
+
+        private void createBtn_Click(object sender, EventArgs e)
+        {
+            Dog dog = new Dog();
+            dog.DogName = dognametxt.Text;
+            dog.DogDesc = dogdescriptiontxt.Text;
+            dog.ImageName = dogimagetxt.Text;
+
+            db.Dogs.Add(dog);
+            db.SaveChanges();
+            MessageBox.Show("Entry saved! :)");
+            dognametxt.Text = "";
+            dogdescriptiontxt.Text = "";
+            dogimagetxt.Text = "";
+        }
+    }
+}
